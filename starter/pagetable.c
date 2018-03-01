9e6c08cff995642aedc91a2757748e63a5bf035d
jun0611 <junzhang0611@hotmail.com>
2018-02-28 23:51:19 -0500

    implemented first method

--- starter/pagetable.c
+++ starter/pagetable.c
@@ -1,3 +1,4 @@
+
 #include <assert.h>
 #include <string.h> 
 #include "sim.h"
@@ -39,8 +40,20 @@ int allocate_frame(pgtbl_entry_t *p) {
 		// All frames were in use, so victim frame must hold some page
 		// Write victim page to swap, if needed, and update pagetable
 		// IMPLEMENTATION NEEDED
-
-
+		pgtbl_entry_t *victim_pte = coremap[frame].pte;
+		unsigned int victim_frame = (victim_pte->frame) >> PAGE_SHIFT;
+		off_t victim_swap_off = victim_pte->swap_off;
+		//init_swap ???
+		//swap out dirty page
+		if(victim_pte->frame & PG_DIRTY) {
+			victim_pte->frame = victim_pte->frame | PG_ONSWAP;
+			evict_dirty_count++;
+		} else {
+			evict_clean_count++;
+		}
+		victim_pte->swap_off = swap_pageout(victim_frame, victim_swap_off)
+		//set valid bit to invalid and 
+		victim_pte->frame = (victim_pte->frame | PG_VALID) ^ PG_VALID;
 	}
 
 	// Record information for virtual page that will now be stored in frame
