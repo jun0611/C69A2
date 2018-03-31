aa1310a9fa3e36f49a8ede9335defec7b0f86f62
Faisal Mohammed <faizal202@gmail.com>
2018-03-30 22:12:07 -0500

    made a few fixes

--- A3/ext2_ls.c
+++ A3/ext2_ls.c
@@ -7,6 +7,7 @@
 #include <sys/mman.h>
 #include "ext2.h"
 #include <string.h>
+#include <errno.h>
 
 #define BLOCK_SIZE 1024
 #define ROOT_INODE_INDEX 1
@@ -17,29 +18,27 @@
 unsigned char *disk;
 char *absolutePath;
 
-void walkPath(char *path){
-	//get the root inode
-	struct ext2_inode *currInode = (struct ext2_inode *)(disk + INODE_TABLE_BLOCK * BLOCK_SIZE) + (INODE_SIZE);
-
-	//Iterate through each directory/file in the path
-	const char delim[1] = "/";
-	char *curr;
-	curr = strtok(path, delim);
-	while(curr != NULL) {
-		currInode = findInodeInDir(currInode, curr);
-		if (currInode == NULL){
-			//There was no file/directory with the specified name
-			return ENOENT;
+struct ext2_dir_entry_2 *findDirEntryInBlock(int blockNum, char *name){
+	int offset = 0;
+	//iterate over all the directory entries in the block
+	while(offset < BLOCK_SIZE){
+		struct ext2_dir_entry_2 *dirEntry = (struct ext2_dir_entry_2 *)(disk + (BLOCK_SIZE * blockNum) + offset);
+		char *entryName = dirEntry->name;
+		//strcmp returns 0 when the two str are equal
+		if(strcmp(entryName, name) == 0){
+			return dirEntry;
 		}
-		curr = strtok(NULL, delim);
+		//increment the offset by the size of the previous dirEntry
+		offset = offset + dirEntry->rec_len;
 	}
+	return NULL;
 }
 
-struct ext2_inode findInodeInDir(struct ext2_inode inode, char *path){
+struct ext2_inode *findInodeInDir(struct ext2_inode *inode, char *path){
 	//ensure inode is a directory
-	if (inode->i_mode & EXT2_S_IFDIR) == EXT2_S_IFDIR){
+	if ((inode->i_mode & EXT2_S_IFDIR) == EXT2_S_IFDIR){
 		int i;
-		struct ext2_dir_entry_2 dirEntry;
+		struct ext2_dir_entry_2 *dirEntry;
 		//iterate over all the direct block pointers
 		for(i = 0; i < NUMBER_OF_SINGLE_POINTERS; i++) {
 			//check if the current block is valid
@@ -58,20 +57,23 @@ struct ext2_inode findInodeInDir(struct ext2_inode inode, char *path){
 	return NULL;
 }
 
-struct ext2_dir_entry_2 findDirEntryInBlock(int blockNum, char *name){
-	int offset = 0;
-	//iterate over all the directory entries in the block
-	while(offset < BLOCK_SIZE){
-		struct ext2_dir_entry_2 *dirEntry = (struct ext2_dir_entry_2 *)(disk + (BLOCK_SIZE * blockNum) + offset);
-		char *entryName = dirEntry->name;
-		//strcmp returns 0 when the two str are equal
-		if(strcmp(entryName, name) == 0){
-			return dirEntry;
+struct ext2_inode * walkPath(char *path){
+	//get the root inode
+	struct ext2_inode *currInode = (struct ext2_inode *)(disk + INODE_TABLE_BLOCK * BLOCK_SIZE) + (INODE_SIZE);
+
+	//Iterate through each directory/file in the path
+	const char delim[1] = "/";
+	char *curr;
+	curr = strtok(path, delim);
+	while(curr != NULL) {
+		currInode = findInodeInDir(currInode, curr);
+		if (currInode == NULL){
+			//There was no file/directory with the specified name
+			//return ENOENT;
 		}
-		//increment the offset by the size of the previous dirEntry
-		offset = offset + dirEntry->rec_len;
+		curr = strtok(NULL, delim);
 	}
-	return NULL;
+	return currInode;
 }
 
 int main(int argc, char **argv){
@@ -82,5 +84,12 @@ int main(int argc, char **argv){
 	int fd = open(argv[1], O_RDWR);
 	disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	absolutePath = argv[2];
-	printPath(absolutePath);
+	struct ext2_inode *inode = walkPath(absolutePath);	
+	if(inode == NULL){
+		printf("NULL");
+	}
+	else {
+		printf("Not NULL");
+	}
+	//printf("%d", inode->i_size);
 }
